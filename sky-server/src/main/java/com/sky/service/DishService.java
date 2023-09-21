2a6199a44d308ae5916431bcdaa3c29bb5b75510
ZLey <1690791641@qq.com>
2023-09-21 16:55:09 +0800

    feat: 菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,10 +2,15 @@ package com.sky.service;
 
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 
 public interface DishService {
 
     public void savewithFlavor(DishDTO dishDTO);
+
+
+    PageResult PageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
