6294750bc0eaa25442f1ecb3600658e33b8d4bcd
ZLey <1690791641@qq.com>
2023-10-12 16:46:46 +0800

    feat: 工作台功能开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -82,4 +83,6 @@ public interface SetmealMapper {
             "from sky_take_out.setmeal_dish sd left join sky_take_out.dish d on sd.dish_id = d.id " +
             "where sd.setmeal_id = #{setmealId}")
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
+    Integer countByMap(Map map);
 }
